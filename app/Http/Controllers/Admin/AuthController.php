8e164207d511d75bb4d72c6faa98534373cc8b07
araserdgn <113991303+araserdgn@users.noreply.github.com>
2024-08-04 23:34:51 +0300

    I have just fixed routes/admin.php for use Group method

--- app/Http/Controllers/Admin/AuthController.php
+++ app/Http/Controllers/Admin/AuthController.php
@@ -10,7 +10,7 @@ class AuthController extends Controller
 {
     //
     public function login() :View{
-        return view('admin/auth/login');
+        return view('admin.auth.login');
     }
 
 
--- routes/admin.php
+++ routes/admin.php
@@ -5,13 +5,16 @@ use App\Http\Controllers\Admin\DashboardController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
-
 Route::get('/admin/login', [AuthController::class, 'login'])->name('admin.login');
 
-Route::get('/admin/dashboard', [DashboardController::class,'index'])
-->name('admin.dashboard.index')
-->middleware('user.type:admin'); // user.type middlewaresine admin olarak manuel değişken atadık
+// 'as' burda name içindeki dosya ismini temsil eder örn: admin.dashboard.index olmasını saglıyr
+Route::group(['middleware' => ['auth','user.type:admin'], 'prefix' => 'admin', 'as' => 'admin.'], function() {
 
+    Route::get('/dashboard', [DashboardController::class,'index'])
+    ->name('dashboard.index');
 
+    //->middleware('user.type:admin'); // user.type middlewaresine admin olarak manuel değişken atadık
+    //gerek kalmadı ama yukard tanımlando
+});
 
 ?>
