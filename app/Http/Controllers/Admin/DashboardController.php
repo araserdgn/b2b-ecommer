911d2fef645b302c3be3970dd8f8a372c17d33a0
araserdgn <113991303+araserdgn@users.noreply.github.com>
2024-08-08 21:23:35 +0300

    I changed dashboard page and I filled in the master.blade with code

--- app/Http/Controllers/Admin/DashboardController.php
+++ app/Http/Controllers/Admin/DashboardController.php
@@ -9,6 +9,6 @@ use Illuminate\Http\Request;
 class DashboardController extends Controller
 {
     public function index(): View {
-        return view('admin.dashboard.index');
+        return view('admin.layouts.master');
     }
 }
